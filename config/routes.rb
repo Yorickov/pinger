c182529c73b07fe116138b7958beae2e3b009253
ashpb <31402193+ashpb@users.noreply.github.com>
2021-05-27 14:24:35 +1000

    Add locale selection via route prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/:locale' => 'landing#index'
   root to: 'landing#index'
 
-  resources :sites
+  scope "(:locale)", locale: /en|ru/ do
+    resources :sites
+  end
 end
