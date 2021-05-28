8fe23cfd3968ede90d8daa9178881072083c169c
ashpb <31402193+ashpb@users.noreply.github.com>
2021-05-29 02:43:41 +1000

    Fix routing order to work with locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/:locale' => 'landing#index'
-  root to: 'landing#index'
-
-  scope '(:locale)', locale: /en|ru/ do
+  scope "(:locale)", locale: /en|ru/ do
     resources :sites
   end
+
+  get '/:locale' => 'landing#index'
+  root to: 'landing#index'
 end
