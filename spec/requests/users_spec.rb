0a5adce5e35c641df75e65409a1b214ffa488c62
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-05-29 13:34:46 +0300

    Add tests with user update

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,20 +2,42 @@
 
 require 'rails_helper'
 
-# TODO: add tests edit page & and update user
-
 RSpec.describe 'Users', type: :request do
   let(:valid_user_params) { attributes_for(:user) }
   let(:invalid_user_params) { attributes_for(:user, :invalid) }
+  subject(:user_signed_in?) { !session['warden.user.user.key'].nil? }
 
-  it 'register user page' do
-    get '/users/sign_up'
-    expect(response).to have_http_status(:success)
+  context 'when user logged in' do
+    it 'register user page' do
+      sign_in User.create(valid_user_params)
+
+      get '/users/sign_up'
+      expect(response).to redirect_to('/')
+    end
+
+    it 'edit user page' do
+      sign_in User.create(valid_user_params)
+
+      get '/users/edit'
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it 'login page' do
-    get '/users/sign_in'
-    expect(response).to have_http_status(:success)
+  context 'when user not logged in' do
+    it 'register user page' do
+      get '/users/sign_up'
+      expect(response).to have_http_status(:success)
+    end
+  
+    it 'login page' do
+      get '/users/sign_in'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'edit user page' do
+      get '/users/edit'
+      expect(response).to redirect_to('/users/sign_in')
+    end
   end
 
   context 'with valid params' do
@@ -24,16 +46,29 @@ RSpec.describe 'Users', type: :request do
       expect(response).to redirect_to('/')
     end
 
-    it 'login / log out' do
+    it 'login' do
       User.create(valid_user_params)
 
       post '/users/sign_in', params: { user: valid_user_params }
       expect(response).to redirect_to('/')
-      expect(session['warden.user.user.key']).not_to be_nil
+      expect(user_signed_in?).to be_truthy
+    end
+
+    it 'log out' do
+      sign_in User.create(valid_user_params)
 
       delete '/users/sign_out'
       expect(response).to redirect_to('/')
-      expect(session['warden.user.user.key']).to be_nil
+      expect(user_signed_in?).to be_falsey
+    end
+
+    it 'update user' do
+      sign_in User.create(valid_user_params)
+      updated_attributes = attributes_for(:user)
+      updated_attributes[:current_password] = updated_attributes[:password]
+
+      put '/users', params: { user: updated_attributes }
+      expect(response).to redirect_to('/')
     end
   end
 
@@ -47,6 +82,15 @@ RSpec.describe 'Users', type: :request do
       post '/users/sign_in', params: { user: valid_user_params }
       expect(response).not_to have_http_status(:redirect)
       expect(session['warden.user.user.key']).to be_nil
+    end
+
+    it 'update user' do
+      sign_in User.create(valid_user_params)
+      updated_attributes = attributes_for(:user)
+      updated_attributes[:current_password] = 'wrong_current_password'
+
+      put '/users', params: { user: updated_attributes }
+      expect(response).not_to have_http_status(:redirect)
     end
   end
 end
