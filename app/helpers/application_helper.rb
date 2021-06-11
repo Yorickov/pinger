aa4f882c19af787d07aff8cff7b696d27cf955a5
yorickov <yorickov@gmail.com>
2021-06-11 19:04:29 +0300

    Add display of ping history

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def format_unix_time(time)
+    Time.zone.at(time).strftime('%d-%m-%Y %H:%M')
+  end
+
+  def format_utc_time(time)
+    time.strftime('%d-%m-%Y %H:%M')
+  end
+
+  def truncate(text, length = 20)
+    text.truncate(length)
+  end
 end
--- app/helpers/sites_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module SitesHelper
-  def format_time(unix_time)
-    Time.zone.at(unix_time).strftime('%d-%m-%Y %H:%M')
-  end
-end
