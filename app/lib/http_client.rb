4f22aed145b50d737833d7dfd8c19ddc9aef2efb
yorickov <yorickov@gmail.com>
2021-06-11 16:57:18 +0300

    Refactor http client

--- app/lib/http_client.rb
+++ app/lib/http_client.rb
@@ -2,41 +2,49 @@
 
 require 'faraday'
 
-module HttpClient
-  class << self
-    # TODO: add request options (timeout, etc as site settings)
-    def call(url)
-      connection = setup_connection(url)
-      make_request(connection)
+class HttpClient
+  DEFAULT_REQUEST_OPTIONS = { timeout: 10 }
+
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def initialize(url, options = {})
+    @options = DEFAULT_REQUEST_OPTIONS.merge(options)
+    @connection = setup_connection(url)
+  end
+
+  def call
+    make_request
+  end
+
+  private
+
+  attr_reader :options, :connection
+
+  def setup_connection(url)
+    Faraday::Connection.new(url, request: options)
+  end
+
+  def make_request
+    start = Time.now.utc
+
+    begin
+      res = connection.get
+      {
+        status: res.status >= 400 ? 'failed' : 'success',
+        response_message: res.reason_phrase,
+        response_time: calc_response_time(start)
+      }
+    # TODO: add special handling?
+    # rescue Faraday::TimeoutError => _e
+    #   { status: 'timeout errored' }
+    rescue StandardError => e
+      { status: 'errored', response_message: e.message }
     end
+  end
 
-    private
-
-    def setup_connection(url)
-      # TODO: take out timeout in options
-      Faraday::Connection.new(url, request: { timeout: 5 })
-    end
-
-    def make_request(connection)
-      start = Time.now.utc
-
-      begin
-        res = connection.get
-        {
-          status: res.status >= 400 ? 'failed' : 'success',
-          response_message: res.reason_phrase,
-          response_time: calc_response_time(start)
-        }
-      # TODO: add special handling?
-      # rescue Faraday::TimeoutError => _e
-      #   { status: 'timeout errored' }
-      rescue StandardError => e
-        { status: 'errored', response_message: e.message }
-      end
-    end
-
-    def calc_response_time(start_time)
-      (Time.now.utc - start_time).in_milliseconds.to_i
-    end
+  def calc_response_time(start_time)
+    (Time.now.utc - start_time).in_milliseconds.to_i
   end
 end
