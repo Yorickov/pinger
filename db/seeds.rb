46228389bd110c015626c0b5121316f10786f33f
yorickov <yorickov@gmail.com>
2021-06-24 21:40:36 +0300

    Fix github ci errors - credentials issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-User.create(
-  email: Rails.application.credentials.admin[:email],
-  password: Rails.application.credentials.admin[:password],
-  password_confirmation: Rails.application.credentials.admin[:password],
-  role: 'admin'
-)
+# TODO: uncomment in deploy
+# User.create(
+#   email: Rails.application.credentials.admin[:email],
+#   password: Rails.application.credentials.admin[:password],
+#   password_confirmation: Rails.application.credentials.admin[:password],
+#   role: 'admin'
+# )
