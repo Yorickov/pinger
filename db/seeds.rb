514ce5a2f6feadf244ab377e3bab0436044b5d0c
yorickov <yorickov@gmail.com>
2021-06-25 23:33:14 +0300

    Remove database seeds

--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-User.create(
-  email: Rails.application.credentials.admin[:email],
-  password: Rails.application.credentials.admin[:password],
-  password_confirmation: Rails.application.credentials.admin[:password],
-  role: 'admin'
-)
